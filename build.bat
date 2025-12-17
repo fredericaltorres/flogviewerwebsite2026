45fae68d3953eea06e869a82addacdba161fd677
frederic.torres@bigtincan.com <fredericaltorres@gmail.com>
2025-12-16 19:45:52 -0500

    Update...

--- build.bat
+++ build.bat
@@ -3,6 +3,7 @@ npm run build
 :: Azure Portal https://portal.azure.com/#@brainshark.onmicrosoft.com/resource/subscriptions/cb3ceaee-cea7-41ca-91be-570db422517f/resourcegroups/fred2026/providers/Microsoft.Web/staticSites/flogviewerwebsite2026/staticsite
 :: Github actions https://github.com/fredericaltorres/flogviewerwebsite2026/actions?query=workflow%3A%22Azure%20Static%20Web%20Apps%20CI%2FCD%22%20branch%3Amain
 :: custome domain CNAME https://www.youtube.com/watch?v=SpQgKfJ87fE
+:: build page https://github.com/fredericaltorres/flogviewerwebsite2026/actions?query=workflow%3A%22Azure%20Static%20Web%20Apps%20CI%2FCD%22%20branch%3Amain
 
 echo see folder 'C:\DVT\fLogViewer.com\flogviewer.2025.web\flogviewerweb2025\out'
 echo purge url https://portal.azure.com/#@fredericaltorreslive.onmicrosoft.com/resource/subscriptions/57646804-986c-47e8-af66-a3abec32e52a/resourceGroups/flogviewer/providers/microsoft.cdn/profiles/flogviewer/endpoints/flogviewer/overview
